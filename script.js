598e74e458cf5353456e1fa582770da9d352024d
WifiRouterYT <ivan@zavyalov.com>
2022-10-19 07:40:37 -0700

    mess around with sticky nav

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // All code is created by WifiRouter
 // Tested on Chrome
 
-var entertxt = null; var enterbtn = null;
+var s = null; var entertxt = null; var enterbtn = null; var navbar = null;
 
 window.addEventListener('load', function() {
     // register elements after site has fully loaded to prevent null errors
     entertxt = document.getElementById('entertxt');
     enterbtn = document.getElementById('enterbtn');
+    navbar = document.getElementById('nav');
+    s = document.getElementById('overlay').style;
     // get rid of text until user clicks button
     entertxt.style.display = "none";
 });
@@ -14,10 +16,11 @@ window.addEventListener('load', function() {
 async function entersite() {
     enterbtn.style.display = "none";
     entertxt.style.display = "block";
-    var s = document.getElementById('overlay').style;
+    
     s.opacity = 1;
     await (function fade(){(s.opacity-=.05)<0?s.display="none":setTimeout(fade,50)})();
     await scaleupanim(entertxt);
+    await gimmenav();
 }
 
 function sleep(ms) {
@@ -31,5 +34,15 @@ async function scaleupanim(object) {
         object.style.fontSize = c;
         object.style.letterSpacing = c-32;
         await sleep(50);
+    }
+}
+
+async function gimmenav() {
+    while(true) {
+        if(s.display = "none") {
+            navbar.style.top = 0;
+            break;
+        }
+        await sleep(100);
     }
 }
